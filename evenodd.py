7f8f7e3dc81c24a236a7a21e12d29eaf63bd6d0b
AKASH1233808 <60962626+AKASH1233808@users.noreply.github.com>
2020-02-15 12:54:06 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,13 @@
+#Even and Odd method
+
+def int(n):
+    r = str(n)
+    r2 = r[::-1]
+    r3 = (r2)
+    print(r3)
+
+def even_odd():
+    if n%2==0:
+        print("even")
+    else
+        print("Odd")
